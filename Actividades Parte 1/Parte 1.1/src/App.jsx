3d7befdbdbf53f46f6199ae5f7facea9f2bc4903
SergioCanas93 <111475866+SergioCanas93@users.noreply.github.com>
2024-11-08 15:32:09 -0500

    Add files via upload

---
+++ Actividades Parte 1/Parte 1.1/src/App.jsx
@@ -0,0 +1,48 @@
+const Header = (props) => {
+  console.log('Header props:', props)
+  return  <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  console.log('Content props:', props)
+  return (
+    <div>
+      <p>{props.parte1} {props.actividades1}</p>
+      <p>{props.parte2} {props.actividades2}</p>
+      <p>{props.parte3} {props.actividades3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log('Total props: ', props)
+  return ( <p>Number of exercises {props.actividades1 + props.actividades2 + props.actividades3} </p> )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 =14
+  
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content 
+        parte1={part1} actividades1={exercises1}
+        parte2={part2} actividades2={exercises2}
+        parte3={part3} actividades3={exercises3}
+      />
+      < Total
+        actividades1={exercises1}
+        actividades2={exercises2}
+        actividades3={exercises3}  
+      />
+    </div>
+  )
+}
+
+export default App
